64c48254d49cfd9341531ca516f16916683af593
adamyajain <apoorvadoc@gmail.com>
2021-02-22 19:57:01 +0530

    commited

---
+++ class.py
@@ -0,0 +1,29 @@
+class Car(object):
+    '''
+        Blueprint for car
+    '''
+
+    def __init__(self, model, color, company, speed_limit)
+        self.color = color
+        self.company = company
+        self.speed_limit = speed_limit
+        self.model = ModuleNotFoundError
+
+    def start(self):
+        print("started")
+    
+    def stop(self):
+        print("stopped")
+    
+    def accelerate(self):
+        print("accelerating...")
+        "accelerator functionality here"
+
+    def change_gear(self, gear_type):
+        print("gear changed")
+        "gear functionality here"
+
+
+audi = Car("A6", "red", "audi", 80)
+
+print(audi.start())
